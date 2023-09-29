06955aa91500cfee3b420881f8fc50b2df67961a
WilliamFilh0 <williamfilho8908@gmail.com>
2023-09-29 16:37:34 -0300

    Verificar se o email já esta cadastrado

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,20 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-  create(request, response) {
+ async create(request, response) {
     const { name, email, password } = request.body;
 
-    if(!name){
-      throw new AppError("Nome é obrigatorio")
+    const database = await sqliteConnection()
+    //Verificar se o email já esta cadastrado
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists){
+      throw new AppError("Este email já esta em uso.")
     }
 
-    response.status(201).json({ name, email, password })
+    response.status(201).json()
   }
 }
 
