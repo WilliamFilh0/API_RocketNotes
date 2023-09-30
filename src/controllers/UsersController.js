202b76c33290e4999211d560f2502e44aff8058c
WilliamFilh0 <williamfilho8908@gmail.com>
2023-09-30 11:31:58 -0300

    Atualizando dados do usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -24,6 +24,43 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    //Funçãp pra buscar usuário 
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id] )
+
+    //Verificar se o usuário existe
+    if (!user) {
+      throw new AppError("Usuário não encontrado")
+    }
+
+    //Verificar se ele quer mudar pra outro email que ja existe
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este e-mail já esta em uso.")
+    }
+
+      user.name = name;
+      user.email = email; 
+
+      await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE ID = ?`,
+        [user.name, user.email, new Date(), id]
+      );
+
+      return response.json()
+    
+    
+  }
 }
 
 module.exports = UsersController;
