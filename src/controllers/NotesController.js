1bf4f84b6de94bb03a80032b2b26ec2a8cfeebe2
WilliamFilh0 <williamfilho8908@gmail.com>
2023-10-27 15:34:51 -0300

    Deletando a nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const knex = require("../database/knex");
 
 class NotesController {
@@ -38,7 +39,7 @@ class NotesController {
 
     const note = await knex("notes").where({ id }).first();
     const tags = await knex("tags").where({ note_id: id }).orderBy("name");
-    const links = await knex("links").where({note_id: id}).orderBy("created_at");
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
 
     return response.json({
       ...note,
@@ -46,6 +47,14 @@ class NotesController {
       links
     });
 
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
   }
 }
 
