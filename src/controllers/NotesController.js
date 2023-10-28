7cde5c9d77019b3e3e29d9427b90dc121e5492f2
WilliamFilh0 <williamfilho8908@gmail.com>
2023-10-28 10:44:22 -0300

    Obtendo tags da nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -34,6 +34,7 @@ class NotesController {
     response.json();
   }
 
+  //Busca e retorna os detalhes de uma nota com suas tags e links associados.
   async show(request, response) {
     const { id } = request.params;
 
@@ -62,28 +63,43 @@ class NotesController {
 
     let notes;
 
+    //consulta  banco para encontrar notas que correspondam as tags 
     if (tags) {
       const filterTags = tags.split(',').map(tag => tag.trim());
 
       notes = await knex("tags")
-      .select([
-        "notes.id",
-        "notes.title",
-        "notes.user_id",
-      ])
-      .where("notes.user_id", user_id)
-      .whereLike("notes.title", `%${title}%`)
-      .whereIn("name", filterTags)
-      .innerJoin("notes", "notes.id", "tags.note_id")
-      .orderBy("notes.title")
- 
+        .select([
+          "notes.id",
+          "notes.title",
+          "notes.user_id",
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title")
+    
+    //Consulta  banco para encontrar notas que correspondam com o ID do usuário e o título
     } else {
       notes = await knex("notes")
         .where({ user_id })
         .whereLike("title", `%${title}%`)
         .orderBy("title");
     }
-    return response.json(notes)
+
+    //Associar as tags de um usuário às suas notas
+    const userTags = await knex("tags").where({ user_id });
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+
+    return response.json(notesWithTags)
 
   }
 }
